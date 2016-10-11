dc2ab1a1859c40b20683d8268d491a07ba03717e
morisse corentin <corentin.morisse@gmail.com>
2016-10-11 13:52:21 +0200

    constructor Tile

--- src/render/Tile.cpp
+++ src/render/Tile.cpp
@@ -4,3 +4,35 @@
  * and open the template in the editor.
  */
 
+namespace render {
+
+  /// class Tile - 
+  class Tile {
+    // Attributes
+  protected:
+    int x;
+    int y;
+    int height;
+    int width;
+    // Operations
+  public:
+      
+      
+    Tile::Tile (int x = 0, int y = 0, int w = 8, int h = 8){
+        this->x = x;
+        this->y = y;
+        this->height = h;
+        this->width = w;
+    };
+    
+    int const Tile::getX ();
+    int const Tile::getY ();
+    int const Tile::getWidth ();
+    int const Tile::getHeight ();
+    void Tile::setX (int x);
+    void Tile::setY (int y);
+    void Tile::setWidth (int w);
+    void Tile::setHeight (int h);
+  };
+
+};
