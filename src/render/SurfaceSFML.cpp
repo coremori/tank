87c6117e8edac302efa9dede68695f717dd57334
morisse corentin <corentin.morisse@gmail.com>
2016-10-11 14:20:51 +0200

    implémentation de la librairie SFML

--- src/render/SurfaceSFML.cpp
+++ src/render/SurfaceSFML.cpp
@@ -4,3 +4,36 @@
  * and open the template in the editor.
  */
 
+#include <vector>
+#include <assert.h> 
+#include <SFML/Graphics.hpp>
+#include "SurfaceSFML.h"
+
+namespace render {
+
+    
+    void SurfaceSFML::loadTexture (const char* image_file){
+        //basicque : "res/Textures/textures.png"
+        
+        if (!texture.loadFromFile(image_file)) 
+        {
+            //error
+        }
+        
+    };
+    
+    void SurfaceSFML::clear (){
+        tiles.clear();
+    };
+    
+    void SurfaceSFML::setSpriteCount (int n){
+        nSprite = n;
+    };
+    
+    void SurfaceSFML::setSpriteTexture (int i, const Tile* tile){
+        assert(i<tiles.size());
+        tiles[i] = tile;
+    };
+  
+
+};
