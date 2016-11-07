bb248c4923f5ea04b161dbbdc5944b0f41a20a55
pierre-bernard <pegase1618@gmail.com>
2016-11-07 17:44:59 +0100

    Rien

--- src/piloteSFML.cpp
+++ src/piloteSFML.cpp
@@ -1,4 +1,3 @@
-
 /* 
  * File:   piloteSFML.cpp
  * Author: corentin
--- src/state/Projectile.cpp
+++ src/state/Projectile.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 #include "state.hpp"
 
 namespace state {
@@ -15,11 +9,10 @@ namespace state {
         return false;
     };
     
-  
-    
     OrientationMissile Projectile::getOrientation () const {
         return Orientation;
     };
+    
     void Projectile::setOrientation (OrientationMissile o){
         Orientation = o;
     };
