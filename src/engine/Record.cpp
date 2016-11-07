45a311b07c4f7c10f36b23fb3a23488f2e520b4e
pierre-bernard <pegase1618@gmail.com>
2016-11-07 18:18:33 +0100

    Record

---
+++ src/engine/Record.cpp
@@ -0,0 +1,37 @@
+/*
+ * Author: Corentin Morisse & Pierre-Bernard Le Roux
+ * Created on November 7, 2016, 6:07 PM
+ *
+ * Part of Projet 
+ */
+#include "Record.h"
+
+namespace engine{
+    Record::Record (state::State& s){
+        
+    }
+    void Record::clear (){
+        
+    }
+    void Record::startRecord (){
+        
+    }
+    void Record::recordOne (ActionListTurn* actions){
+        
+    }
+    void Record::stopRecord (){
+        
+    }
+    void Record::startReplay (){
+        
+    }
+    bool Record::replayOne (){
+        return false;
+    }
+    void Record::startRollback (){
+        
+    }
+    bool Record::rollbackOne (){
+        return false;
+    }
+}
