e1ea036188c359978b6dd2d987cbd53fb53aaf84
pierre-bernard <pegase1618@gmail.com>
2016-11-10 17:09:53 +0100

    ai

---
+++ src/ai/AI.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: Corentin Morisse & Pierre-Bernard Le Roux
+ * Created on November 10, 2016, 5:00 PM
+ *
+ * Part of tank 
+ */
+#include "AI.h"
+
+namespace ai {
+
+    AI::AI (const state::State& mainState){
+        
+    };
+    
+    AI::~AI (){
+        
+    };
+    
+    bool const AI::nextLocation (int idx, int& x, int& y){
+        
+    };
+    
+    void AI::run (engine::CommandSet& commands){
+        
+    };
+  
+
+};
