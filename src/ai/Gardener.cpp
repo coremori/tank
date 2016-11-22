e3c75b270d9342a43677777b92a95bff9bb9dc20
pierre-bernard <pegase1618@gmail.com>
2016-11-22 22:41:35 +0100

    Rien

---
+++ src/ai/Gardener.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: Corentin Morisse & Pierre-Bernard Le Roux
+ * Created on November 22, 2016, 10:33 PM
+ *
+ * Part of tank 
+ */
+#include "Gardener.h"
+
+namespace ai{
+    Gardener::Gardener (int depthMax, state::State* state){
+        
+    }
+    
+    
+    
+    void Gardener::Evaluate (){
+        
+    }
+    
+    int Gardener::bestMove (){
+        return 0;
+    }
+    int Gardener::minimax_rec (Node* Node){
+        
+        return 0;
+    }
+    int Gardener::minimax_rec_max (Node* Node){
+        return 0;
+    }
+    int Gardener::minimax_rec_min (Node* Node){
+        return 0;
+    }
+
+}
