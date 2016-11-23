3a7467b82c1a305c06cf8b54ab4c43d493da037d
morisse corentin <corentin.morisse@gmail.com>
2016-11-23 16:26:26 +0100

    add file

---
+++ src/ai/TreeAI.cpp
@@ -0,0 +1,23 @@
+/* 
+ * Author: Corentin Morisse & Pierre-Bernard Le Roux 
+ * Created on November 21, 2016, 2:01 PM
+ *
+ * Part of tank */
+
+
+#include "TreeAI.h"
+
+namespace ai{
+
+    TreeAI::TreeAI(state::State* state, int character) : EvolvedAI(state,character) {
+
+    }
+        
+    void TreeAI::run(engine::CommandSet& commands) {
+
+    }
+
+
+}
+
+/*press ctrl+space for create function define in .h*/
