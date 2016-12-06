09148a3177c77a968536ecd109f154f29d054b8d
morisse corentin <corentin.morisse@gmail.com>
2016-12-06 15:39:33 +0100

    Debut de l'implémentation du code

--- src/webservice/AbstractService.cpp
+++ src/webservice/AbstractService.cpp
@@ -7,7 +7,7 @@
 
 #include "AbstractService.hpp"
 
-AbstractService::AbstractService (const string& pattern) {
+AbstractService::AbstractService (const string& pattern) {//patern == url ici (interne au serveur)
     setPattern(pattern);
 }
 
@@ -22,6 +22,8 @@ const string& AbstractService::getPattern () const {
 void AbstractService::setPattern (const string& pattern) {
     this->pattern = pattern;
 }
+
+//toutes les méthodes suivante sont virtuelle et ne sont défini ici que pour permettre une réutilisation facile de la classe : Elle gére tout les types de requete basique (post,put,get et delete) de la manière la plus simple : je repond non implémenté pour toutes, et j'ai pas à les définir dans les classes filles
 
 HttpStatus AbstractService::get (Json::Value& out, int id) const {
     throw ServiceException(HttpStatus::NOT_IMPLEMENTED,"Non implanté");
